70b2eb517ffc047d9d7a8ef5d2489bd2b002bf95
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2023-10-30 11:44:48 -0300

    up

---
+++ WebSolution/WebDBService/Program.cs
@@ -0,0 +1,15 @@
+using Microsoft.AspNetCore.Builder;
+using Microsoft.Extensions.DependencyInjection;
+using WebDBService.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddGrpc();
+var app = builder.Build();
+
+app.MapGrpcService<GreeterService>();
+app.MapGet("/",
+    () =>
+        "Communication with gRPC endpoints must be made through a gRPC client. To learn how to create a client, visit: https://go.microsoft.com/fwlink/?linkid=2086909");
+app.Run();

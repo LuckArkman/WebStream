6034ad1f3063bcbc4bfa677149d4701cbf07255d
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2023-11-01 16:01:31 -0300

    up

--- WebSolution/Catalog.Domain/Entity/Category.cs
+++ WebSolution/Catalog.Domain/Entity/Category.cs
@@ -2,12 +2,19 @@ namespace Catalog.Domain.Entity;
 
 public class Category
 {
-    public Category(string _name, string _description)
+    public Category(string _name, string _description, bool isActive = true)
     {
+        Id = Guid.NewGuid();
         Name = _name;
         Description = _description;
+        IsActive = isActive;
+        createTime = DateTime.Now;
 
     }
+    public Guid Id { get; set; }
     public string Name { get; set; }
     public string Description { get; set; }
+    public bool IsActive { get; set; }
+
+    public DateTime createTime { get; set; }
 }
--- WebSolution/TestProject/Entity/Categorys/CategoryTest.cs
+++ WebSolution/TestProject/Entity/Categorys/CategoryTest.cs
@@ -15,12 +15,59 @@ public class CategoryTest
             Name = "category name",
             Description = "category Description",
         };
-        //Act
-        var category = new Category(validData.Name,
-                                    validData.Description);
+        var dateTimeBefore = DateTime.Now;
+        var category = new Category(validData.Name, validData.Description);
+        var dateTimeAfter = DateTime.Now;
         Assert.NotNull(category);
         Assert.Equal(validData.Name, category.Name);
         Assert.Equal(validData.Description, category.Description);
-        
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.createTime);
+        Assert.True(category.createTime > dateTimeBefore);
+        Assert.True(category.createTime < dateTimeAfter);
+        Assert.True(category.IsActive);
+    }
+
+    [Theory(DisplayName = nameof(InstantiateIsActive))]
+    [Trait("Domain", "Category - Aggregates")]
+    [InlineData(true)]
+    [InlineData(false)]
+    public void InstantiateIsActive(bool IsActive)
+    {
+        //Arrange
+        var validData = new
+        {
+            Name = "category name",
+            Description = "category Description",
+        };
+        var dateTimeBefore = DateTime.Now;
+        //Act
+        var category = new Category(validData.Name, validData.Description, IsActive);
+        var dateTimeAfter = DateTime.Now;
+        Assert.NotNull(category);
+        Assert.Equal(validData.Name, category.Name);
+        Assert.Equal(validData.Description, category.Description);
+        Assert.NotEqual(default(Guid), category.Id);
+        Assert.NotEqual(default(DateTime), category.createTime);
+        Assert.True(category.createTime > dateTimeBefore);
+        Assert.True(category.createTime < dateTimeAfter);
+        Assert.Equal(IsActive , category.IsActive);
+    }
+
+    [Fact(DisplayName = nameof(InstantiateErrorNameIsEmpty))]
+    [Trait("Domain", "Category - Aggregates")]
+
+    public void InstantiateErrorNameIsEmpty()
+    {
+        Action action = () => Category("category name", null);
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Description shoud not be empty or null", exception.message);
+    }
+
+    public void InstantiateErrorWhenNameIsLessThan3Characters()
+    {
+        Action action = () => Category("category name", null);
+        var exception = Assert.Throws<EntityValidationException>(action);
+        Assert.Equal("Description shoud not be empty or null", exception.message);
     }
 }

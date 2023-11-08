d39e503e5529b9edea9e9074a12a30acdbc5c34e
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2023-11-07 23:31:20 -0300

    up

--- WebSolution/TestProject/CreateCategorys/CreateCategoryTest.cs
+++ WebSolution/TestProject/CreateCategorys/CreateCategoryTest.cs
@@ -48,7 +48,7 @@ public class CreateCategoryTest
         output.createTime.Should().NotBeSameDateAs(default(DateTime));
     }
     [Theory(DisplayName = nameof(CreateCategoryThrowWhenInstantiate))]
-    [Trait("Domain", "CreateCategory - Use Cases")]
+    [Trait("CreateCategoryTest", "CreateCategory - Use Cases")]
     [MemberData(nameof(GetInvalidInputs))]
     public async void CreateCategoryThrowWhenInstantiate(CreateCategoryInput input, string exception)
     {
@@ -62,11 +62,98 @@ public class CreateCategoryTest
         Func<Task> action = async () => await useCase.Handle(input, CancellationToken.None);
         await action.Should().ThrowAsync<EntityValidationException>().WithMessage(exception);
     }
+    
+    [Fact(DisplayName = nameof(CreateCategoryOnlyName))]
+    [Trait("CreateCategoryTest", "CreateCategory - Use Cases")]
+    public async void CreateCategoryOnlyName()
+    {
+        var categoryMock = _fixture.GetcategoryMock();
+        var unityOfWorkMock = _fixture.GetunityOfWorkMock();
+
+        var useCase = new CreateCategory(
+            categoryMock.Object,
+            unityOfWorkMock.Object
+        );
+        
+        var input = new CreateCategoryInput(
+            null,
+            _fixture.GetValidCategoryName(),
+            "",
+            true,
+            null
+            
+        );
+        
+        var output = await useCase.Handle(input, CancellationToken.None);
+        
+        categoryMock.Verify(repository => repository.Insert(It.IsAny<Category>(),
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+        
+        unityOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()),
+            Times.Once());
+        
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be("");
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.createTime.Should().NotBeSameDateAs(default(DateTime));
+    }
+    
+    [Fact(DisplayName = nameof(CreateCategoryOnlyNameAndDescription))]
+    [Trait("CreateCategoryTest", "CreateCategory - Use Cases")]
+    public async void CreateCategoryOnlyNameAndDescription()
+    {
+        var categoryMock = _fixture.GetcategoryMock();
+        var unityOfWorkMock = _fixture.GetunityOfWorkMock();
+
+        var useCase = new CreateCategory(
+            categoryMock.Object,
+            unityOfWorkMock.Object
+        );
+        
+        var input = new CreateCategoryInput(
+            null,
+            _fixture.GetValidCategoryName(),
+            _fixture.GetValidCategoryDescription(),
+            true,
+            null
+            
+        );
+        
+        var output = await useCase.Handle(input, CancellationToken.None);
+        
+        categoryMock.Verify(repository => repository.Insert(It.IsAny<Category>(),
+                It.IsAny<CancellationToken>()),
+            Times.Once());
+        
+        unityOfWorkMock.Verify(uow => uow.Commit(It.IsAny<CancellationToken>()),
+            Times.Once());
+        
+        output.Should().NotBeNull();
+        output.Name.Should().Be(input.Name);
+        output.Description.Should().Be(input.Description);
+        output.IsActive.Should().Be(input.IsActive);
+        output.Id.Should().NotBeEmpty();
+        output.createTime.Should().NotBeSameDateAs(default(DateTime));
+    }
 
     public static IEnumerable<object[]> GetInvalidInputs()
     {
         var _fixture = new CreateCategoryTestFixture();
         var invalidInput = new List<object[]>();
+        var inputNullName = _fixture.GetInput();
+        while (inputNullName.Name != null)
+        {
+            inputNullName.Name = null;
+
+        }
+        invalidInput.Add(new object[]
+        {
+            inputNullName,
+            "Name should not be empty or null"
+        });
         var input = _fixture.GetInput();
         while (input.Name.Length > 3)
         {
@@ -87,6 +174,28 @@ public class CreateCategoryTest
         {
             inputName,
             "Name should be less or equal 255 characters Long"
+        });
+        
+        var inputDesc = _fixture.GetInput();
+        while (inputDesc.Description != null)
+        {
+            inputDesc.Description = null;
+        }
+        invalidInput.Add(new object[]
+        {
+            inputDesc,
+            "Description should not be null"
+        });
+        
+        var inputDescMax = _fixture.GetInput();
+        while (inputDescMax.Description.Length < 10000)
+        {
+            inputDescMax.Description = $"{inputDescMax.Description} {_fixture.faker.Commerce.ProductDescription()}";
+        }
+        invalidInput.Add(new object[]
+        {
+            inputDescMax,
+            "Description should be less or equal 10000 characters long"
         });
         
         return invalidInput;

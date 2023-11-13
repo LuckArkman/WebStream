6893b5a3063c823a97f297336146108726a7b5b7
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2023-11-13 20:11:37 -0300

    Update CategoryRepositoryTest.cs

--- WebSolution/Catalog.Infra/Repositories/CategoryRepositoryTest.cs
+++ WebSolution/Catalog.Infra/Repositories/CategoryRepositoryTest.cs
@@ -1,7 +1,14 @@
+using System.Collections.ObjectModel;
+using Xunit;
 namespace Catalog.Infra.Repositories
 {
+    [Collection(nameof(CategoryRepositoryTestFixture))]
     public class CategoryRepositoryTest
     {
+        readonly CategoryRepositoryTestFixture _fixture;
+
+        public CategoryRepositoryTest(CategoryRepositoryTestFixture fixture)
+        => _fixture = fixture;
         
     }
 }

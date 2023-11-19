7d22a6e9cc5665a00b51a31c8cedbaac1750b612
Mauricio da Paixão Costa <mauriciopaixao21@gmail.com>
2023-11-18 23:26:43 -0300

    up

--- WebSolution/Catalog.Infra/Application/GetCategory/GetCategory.cs
+++ WebSolution/Catalog.Infra/Application/GetCategory/GetCategory.cs
@@ -2,14 +2,18 @@ using Catalog.Application.Common;
 using Catalog.Application.GetCategoryTest.Categorys;
 using Catalog.Application.UseCases.Category;
 using Catalog.Data.Configurations;
+using Catalog.Domain.Repository;
 
 namespace Catalog.Infra.Application.GetCategory;
 
 public class GetCategory : IGetCategory
 {
+    private readonly ICategoryRepository _categoryRepository;
     private readonly CatalogDbContext _context;
 
     public GetCategory(CatalogDbContext categoryDb) => _context = categoryDb;
+
+    public GetCategory(ICategoryRepository categoryRepository) => _categoryRepository = categoryRepository;
 
     public async Task<CategoryModelOutput> Handle(GetCategoryInput request, CancellationToken cancellationToken)
     {
--- WebSolution/Catalog.Infra/Application/GetCategory/GetCategoryTest.cs
+++ WebSolution/Catalog.Infra/Application/GetCategory/GetCategoryTest.cs
@@ -1,4 +1,5 @@
 using Catalog.Application.GetCategoryTest.Categorys;
+using Catalog.Domain.Exceptions;
 using Catalog.Infra.Repositories;
 using FluentAssertions;
 using Moq;
@@ -18,6 +19,7 @@ public class GetCategoryTest
     public async Task GetCategory()
     {
         var _Id = Guid.NewGuid().ToString();
+        var repositoryMock = _fixture.GetcategoryMock();
         var _categoryDB = _fixture.CreateDBContext(false, _Id);
         var _category = _fixture.GetValidCategory();
         _categoryDB.Categories.Add(_category);
@@ -35,6 +37,26 @@ public class GetCategoryTest
         output.IsActive.Should().Be(_category.IsActive);
         output.Id.Should().Be(_category.Id);
         output.createTime.Should().Be(_category.createTime);
+    }
+    
+    [Fact(DisplayName = nameof(NotFoundExceptionWhenCategoryDoesntExist))]
+    [Trait("GetCategoryTestFixture", "CategoryRepositoryTest - Infra")]
+    public async Task NotFoundExceptionWhenCategoryDoesntExist()
+    {
+        var _Id = Guid.NewGuid().ToString();
+        var _repositoryMock = _fixture.GetcategoryMock();
+        var _categoryDB = _fixture.CreateDBContext(false, _Id);
+        var _category = _fixture.GetValidCategory();
+        _categoryDB.Categories.Add(_category);
+        _categoryDB.SaveChanges();
+        
+        var _categoryRepository = new CategoryRepository(_categoryDB);
+        var input = new GetCategoryInput(Guid.NewGuid());
+        var useCase = new GetCategory(_categoryDB);
 
+        var task = async ()
+            => await useCase.Handle(input, CancellationToken.None);
+
+        await task.Should().ThrowAsync<NotFoundException>().WithMessage($"Category '{input.Id}' not found.");
     }
 }
